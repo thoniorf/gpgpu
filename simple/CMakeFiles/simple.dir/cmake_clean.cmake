1b69ff10205ebba2e4f046af7a09d62f9a6c5bac
Antonio <thoniorf@gmail.com>
2017-10-10 11:45:53 +0200

    exercises sync

---
+++ simple/CMakeFiles/simple.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple.dir/simple.cpp.o"
+  "CMakeFiles/simple.dir/glad.c.o"
+  "simple.pdb"
+  "simple"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/simple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
